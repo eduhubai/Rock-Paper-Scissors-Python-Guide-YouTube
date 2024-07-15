fe439fe82f0726d7a4872ff3999d356648a5cdcd
eduhubai <165917035+eduhubai@users.noreply.github.com>
2024-07-15 21:07:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+# Importing Libraries
+
+
+# Let's start by welcoming the player to the game with a simple print statement."
+
+
+# Initialize scores and gameplay variables
+
+
+# List of valid choices for the game
+
+
+# Main game loop
+
+    # Inner Loop for Valid User Input
+    
+    
+
+        #Display User Choice ASCII Art
+     
+     
+     
+     
+
+        # Check if the choice is one of the valid options
+
+
+
+
+
+
+    # Generate computer's choice randomly from the valid options
+
+        
+        
+        
+        
+        
+
+    # Compare player's and computer's choices to determine the outcome
+    
+        
+        
+        
+        
+        
+        
+        
+        
+
+    # Display the current score after each round
+
+
+
+
+
+
+    # Ask the player if they want to play another round
+
+
+
+
+
+
+# Final score and game outcome
+
+    
+    
+    
